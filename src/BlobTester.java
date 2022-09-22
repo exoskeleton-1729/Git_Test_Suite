a3609a760c5611b675cfb7a76c5d8ff5e4a115a0
Anika Iyer <anika.sule.iyer@gmail.com>
2022-09-22 10:13:54 -0700

    Separated BlobTester
    Exactly what it sounds like.

--- src/BlobTester.java
+++ src/BlobTester.java
@@ -1,5 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -8,15 +16,25 @@ class BlobTester {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		Path p = Paths.get("junit.txt");
+        try {
+            Files.writeString(p, "Will Tester", StandardCharsets.ISO_8859_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		File f = new File("junit.txt");
+		f.delete();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test() throws NoSuchAlgorithmException, IOException {
+		Blob b = new Blob("junit.txt");
+		File file  = new File("objects/b34bcfc4d490f93ed9cf4e560c608b58cd688c24");
+		assertTrue(file.exists());
 	}
 
 }
